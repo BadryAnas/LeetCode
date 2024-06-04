c3c440323b845be0a617fb135d4c3ee47d1cc8f2
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2024-06-04 17:48:48 +0300

    Time: 1210 ms (93.81%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,7 @@
 select mngr.name   
 from employee emp , employee mngr
 where mngr.id = emp.managerId
-group by mngr.id, mngr.name  
+group by emp.managerId, mngr.name  
 having count(emp.managerId) >= 5 
 
 -----------------------------
