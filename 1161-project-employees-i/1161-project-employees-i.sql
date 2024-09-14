8bf12325610e6dd464536fb8264625b18215bd08
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2024-09-14 21:39:48 +0300

    Time: 1305 ms (63.5%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -1,4 +1,4 @@
 select p.project_id  , round(  avg(e.experience_years * 1.0 ) , 2) as average_years
-from Project p inner join Employee e
+from Project p  join Employee e
 on p.employee_id = e.employee_id 
 group by p.project_id  
