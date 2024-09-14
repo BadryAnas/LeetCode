42d1e308a04c5813c6283562cc6e122eb1b26f4b
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2024-09-14 21:57:36 +0300

    Time: 3242 ms (37.69%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,9 @@
-select r.contest_id , round(  ( count( r.contest_id)*1.0  /( select count(*) from  Users) ) * 100  ,2)  as percentage
+DECLARE @total_users float;
+
+SELECT @total_users = COUNT(*) 
+FROM Users;
+
+select r.contest_id , round(  ( count( r.contest_id)*1.0  / @total_users ) * 100  ,2)  as percentage
 from Users u inner join Register r
 on u.user_id  = r.user_id 
 group by r.contest_id
