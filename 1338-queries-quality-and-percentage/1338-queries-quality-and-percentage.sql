415c21f53644b3ca2a215a05c7cafe28a2fde414
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2024-09-15 18:44:23 +0300

    Time: 1337 ms (19.97%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
  select query_name , 
         round( avg ( rating * 1.0 / position ) , 2 ) as quality ,
-        round( count(case when rating<3 then 1 else null end) * 100.0 / count(*) , 2) as poor_query_percentage 
+        round( sum(case when rating<3 then 1 else 0 end) * 100.0 / count(*) , 2) as poor_query_percentage 
 from Queries
 group by query_name 
 having query_name is not null
