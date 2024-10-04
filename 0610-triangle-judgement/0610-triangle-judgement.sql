6f8f35e5cc7e7749993394624dcae1a6a9c5e460
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2024-10-04 18:02:43 +0300

    Time: 295 ms (58.88%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,3 +1,3 @@
 /* Write your T-SQL query statement below */
-select * ,  (case when (x + y > z and z + x > y and z + y > x) then 'Yes' else 'No' end)  as triangle 
+select * , case when (x + y > z and z + x > y and z + y > x) then 'Yes' else 'No' end  as triangle 
 from Triangle 
