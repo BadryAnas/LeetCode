3a35723a1984ba0723fdef789daff413274b6d27
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2024-10-09 20:47:09 +0300

    Time: 351 ms (37.11%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+with newTable (id)
+as
+(
+    select requester_id as id from RequestAccepted
+    union all 
+    select accepter_id as id from RequestAccepted
+)
+select top(1) id, count(id) as num
+from newTable 
+group by id
+order by num desc
