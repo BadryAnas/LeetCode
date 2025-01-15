b80951bdb19b76fc5853a8ce1c4a70eec82af42e
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2025-01-15 23:19:02 +0200

    Time: 531 ms (5.05%), Space: 24.8 MB (73.67%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> res;
+        set<string> stRes;
+        map<string , int>mp;
+
+        if(s.size() < 11)
+            return res;
+        
+        string str = s.substr(0, 10);
+        mp[str]++;
+
+        for(int  j = 10 ; j < s.size() ;j++)
+        {
+            str.erase(0 ,1);
+            cout<<str<<" ";
+            str = str + s[j];
+            cout<<str<<" lol ";
+            mp[str]++;
+        }
+
+        for(auto &[key, value] : mp)
+        {
+            if ( value > 1 )
+            {
+                stRes.insert(key);
+            }
+        }
+
+        for(auto x : stRes)
+        {
+            res.push_back(x);
+        }
+
+        return res;
+
+
+
+
+
+    }
+};
