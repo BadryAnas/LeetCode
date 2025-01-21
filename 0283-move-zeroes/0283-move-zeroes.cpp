9231653b9b8c79879f196849e7bb85fb3f6234ec
Badry Anas <91754039+BadryAnas@users.noreply.github.com>
2025-01-21 18:34:38 +0200

    Time: 64 ms (7.12%), Space: 23.8 MB (74.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>num;
-        vector<int>zeros;
-        int ctr = 0, ctr2 = 0 , ctr3 = 0;
-        for(int i = 0 ; i < nums.size() ; i++)
+        // vector<int>num;
+        // vector<int>zeros;
+        // int ctr = 0, ctr2 = 0 , ctr3 = 0;
+        // for(int i = 0 ; i < nums.size() ; i++)
+        // {
+        //     if(nums[ i ] == 0)
+        //         zeros.push_back(nums [ i ]);
+        //     else
+        //         num.push_back(nums [ i ]);
+        // } 
+        // for( ; ctr2 < num.size() ; ctr2++, ctr++)
+        // {
+        //     nums[ctr] = num[ctr2];
+        // }
+        // for( ; ctr3 < zeros.size() ; ctr3++, ctr++)
+        // {
+        //     nums[ctr] = zeros[ctr3];
+        // }
+
+        for(int i = 0 ; i < nums.size()  ;i++)
         {
-            if(nums[ i ] == 0)
-                zeros.push_back(nums [ i ]);
-            else
-                num.push_back(nums [ i ]);
-        } 
-        for( ; ctr2 < num.size() ; ctr2++, ctr++)
-        {
-            nums[ctr] = num[ctr2];
+            if(nums[i] == 0)
+            {
+                for(int j = i + 1; j < nums.size() ; j++)
+                {
+                    if(nums[j] != 0)
+                    {
+                        swap(nums[i] , nums[j]);
+                        break;
+                    }
+                }
+            }
         }
-        for( ; ctr3 < zeros.size() ; ctr3++, ctr++)
-        {
-            nums[ctr] = zeros[ctr3];
-        }
+
 
     }
 };
